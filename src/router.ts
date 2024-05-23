9dc8f1818a218dabd6049fbab193da018c3f2fac
kazi031 <kazirafidraiyan031@gmail.com>
2024-05-23 17:33:34 +0600

    Validation Check

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import {Router} from 'express'
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors, validateUpdateProperty } from './modules/middleware';
 
 const router = Router()
 
@@ -9,8 +11,12 @@ router.get('/property', (req,res) => {
 })
 router.get('/property/:id', () => {})
 
-router.put('/property/:id', () => {})
-router.post('/property', () => {})
+router.put('/property/:id', 
+    validateUpdateProperty,
+    handleInputErrors, 
+    (req, res) => {}
+)
+router.post('/property', body('title').isString(), handleInputErrors, (req, res) => {})
 
 router.delete('/property/:id', () => {})
 
